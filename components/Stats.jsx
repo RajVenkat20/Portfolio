1c76ad0c0e9145d11b01f154b5f3d8b24fbbf102
Raj Venkat Reddy Mavuram <mrajvenkatreddy@gmail.com>
2025-01-06 16:35:39 -0600

    Initial commit

---
+++ components/Stats.jsx
@@ -0,0 +1,28 @@
+"use client";
+
+import CountUp from "react-countup";
+
+const stats = [
+    {
+        num: 3,
+        text: "Years of experience",
+    },
+    {
+        num: 26,
+        text: "Years of experience",
+    },
+    {
+        num: 8,
+        text: "Years of experience",
+    },
+    {
+        num: 500,
+        text: "Years of experience",
+    },
+];
+
+const Stats = () => {
+    return <div>Stats</div>;
+};
+
+export default Stats;
