9d7253407ddf91c7eb3d3e39ef0ed7a5e45fad29
Raj Venkat Reddy Mavuram <mrajvenkatreddy@gmail.com>
2025-01-27 10:07:35 -0600

    Added "+" and "~" to Stats section component values

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -36,6 +36,8 @@ const Stats = () => {
                   end={item.num}
                   duration={5}
                   delay={2}
+                  prefix={item.num == 4 ? "~" : ""}
+                  suffix={item.num == 15 || item.num == 250 ? "+" : ""}
                   className="text-4xl xl:text-6xl font-extrabold"
                 />
                 <p className={`${item.text.length < 15 ? "max-w-[100px]" : "max-w-[150px]"} leading-snug text-white/80`}>{item.text}</p>
