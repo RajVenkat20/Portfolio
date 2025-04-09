d444e5596c3afaf2cebf3f2b019b46422a7bfeed
Raj Venkat Reddy Mavuram <mrajvenkatreddy@gmail.com>
2025-04-08 23:21:09 -0500

    Fixed spacing in Experience section

--- app/experience/page.jsx
+++ app/experience/page.jsx
@@ -109,8 +109,8 @@ const Experience = () => {
                           key={index}
                           className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1"
                         >
-                          <span className="text-accent">{item.duration}</span>
-                          <h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-left">
+                          <span className="text-accent min-h-[40px]">{item.duration}</span>
+                          <h3 className="text-xl max-w-[260px] min-h-[40px] text-center lg:text-left">
                             {item.position}
                           </h3>
                           <div className="flex items-center gap-3">
