9613ec130c1255d43b01d8273ab29676c27288e5
Raj Venkat Reddy Mavuram <mrajvenkatreddy@gmail.com>
2025-04-08 23:14:13 -0500

    Removed Hire Me button

--- components/Header.jsx
+++ components/Header.jsx
@@ -18,9 +18,9 @@ const Header = () => {
             {/* desktop nav & hire me button*/}
             <div className="hidden xl:flex items-center gap-8">
                 <Nav />
-                <Link href="/contact">
+                {/* <Link href="/contact">
                     <Button>Hire Me</Button>
-                </Link>
+                </Link> */}
             </div>
 
             {/* mobile nav */}
